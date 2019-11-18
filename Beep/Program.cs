11657a5db1cebf1766a65b4ef7be86c88af7957f
mark <mark@DESKTOP-A3C5HKC>
2019-11-18 10:55:49 +0100

    Initial checkin

---
+++ Beep/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Beep
+{
+    using System.Media;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemSounds.Beep.Play();
+        }
+    }
+}
